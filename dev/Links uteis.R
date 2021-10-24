51d1e43a095992a1ec11423dc2cb237d2d3a98c8
Bruno Tebaldi de Queiroz Barbosa <bruno.barbosa@fgv.br>
2021-10-24 19:58:08 -0300

    Links uteis

---
+++ dev/Links uteis.R
@@ -0,0 +1,16 @@
+#' Links uteis
+#'
+#'
+#' https://petrobras.com.br/data/files/9B/E7/79/DC/F3B8A710E563BA97B8E99EA8/Precos%20Gasolina%20e%20Diesel%20por%20Estado-05072021.pdf
+#' 
+#' https://www.gov.br/anp/pt-br/assuntos/precos-e-defesa-da-concorrencia/precos/precos-revenda-e-de-distribuicao-combustiveis/serie-historica-do-levantamento-de-precos
+#' 
+#' https://www.gov.br/anp/pt-br/assuntos/royalties-e-outras-participacoes/royalties
+#' 
+#' https://www.gov.br/anp/pt-br/assuntos/precos-e-defesa-da-concorrencia/precos/precos-revenda-e-de-distribuicao-combustiveis/levantamento-de-precos-de-combustiveis
+#' 
+#' https://petrobras.com.br/pt/nossas-atividades/precos-de-venda-de-combustiveis/
+#' 
+#' https://duvidasgasolina.petrobras.com.br/
+#' 
+#' 
